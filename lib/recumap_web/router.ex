c978356bd65b79b3f72a7eec85887fc21685f19e
belcattaneo <belcattaneo@fiqus.coop>
2020-04-10 17:14:21 -0300

    #1 Pow auth token for api

--- lib/recumap_web/router.ex
+++ lib/recumap_web/router.ex
@@ -2,26 +2,31 @@ defmodule RecumapWeb.Router do
   use RecumapWeb, :router
   use Pow.Phoenix.Router
 
-  pipeline :protected do
-    plug Pow.Plug.RequireAuthenticated,
-      error_handler: Pow.Phoenix.PlugErrorHandler
-  end
+
 
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
-    plug :protect_from_forgery
+    #plug :protect_from_forgery
     plug :put_secure_browser_headers
+  end
+
+  pipeline :protected do
+    plug Pow.Plug.RequireAuthenticated, error_handler: Pow.Phoenix.PlugErrorHandler
   end
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug RecumapWeb.APIAuthPlug, otp_app: :recumap
+  end
+
+  pipeline :api_protected do
+    plug Pow.Plug.RequireAuthenticated, error_handler: RecumapWeb.APIAuthErrorHandler
   end
 
   scope "/" do
     pipe_through :browser
-
     pow_routes()
   end
 
@@ -36,8 +41,16 @@ defmodule RecumapWeb.Router do
     resources "/orgs", OrgController
   end
 
-  scope "/api", RecumapWeb do
+  scope "/api", RecumapWeb.API do
     pipe_through :api
+
+    resources "/registration", RegistrationController, singleton: true, only: [:create]
+    resources "/session", SessionController, singleton: true, only: [:create, :delete]
+    post "/session/renew", SessionController, :renew
+  end
+
+  scope "/api", RecumapWeb do
+    pipe_through [:api, :api_protected]
 
     get "/resources", ResourceApiController, :index
     resources "/orgs", OrgApiController
